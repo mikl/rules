f762cd2d438b835b3e8e41e1b1825b672800c0cc
Diarmuid <diarmuid@clickclickclick.co.uk>
2015-01-27 16:39:05 +0000

    Some more changes

--- tests/src/Integration/Action/EntityPropertyFetchTest.php
+++ tests/src/Integration/Action/EntityPropertyFetchTest.php
@@ -8,6 +8,14 @@
 namespace Drupal\Tests\rules\Integration\Action;
 
 use Drupal\Tests\rules\Integration\RulesEntityIntegrationTestBase;
+use Drupal\Core\TypedData\TypedDataManager;
+use Drupal\rules\Context\ContextDefinition;
+use Drupal\rules\Engine\RulesActionBase;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\Sql\SqlContentEntityStorage;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
  * @coversDefaultClass \Drupal\rules\Plugin\Action\EntityPropertyFetch
@@ -22,11 +30,65 @@ class EntityPropertyFetchTest extends RulesEntityIntegrationTestBase {
    */
   protected $action;
 
+  protected $entityStorage;
+
   /**
    * {@inheritdoc}
    */
   public function setUp() {
     parent::setUp();
+
+    $this->enableModule('node');
+
+    $factory = $this->getMockBuilder('Drupal\Core\Entity\Query\QueryFactory')
+        ->disableOriginalConstructor()
+        ->getMock();
+
+    $statement = $this->getMockBuilder('Drupal\Core\Database\Driver\fake\FakeStatement')
+        ->disableOriginalConstructor()
+        ->getMock();
+
+    $statement->expects($this->any())
+        ->method('fetchObject')
+        ->will($this->returnCallback(array($this, 'fetchObjectCallback')));
+
+    $select = $this->getMockBuilder('Drupal\Core\Database\Query\Select')
+        ->disableOriginalConstructor()
+        ->getMock();
+
+    $select->expects($this->any())
+        ->method('fields')
+        ->will($this->returnSelf());
+
+    $select->expects($this->any())
+        ->method('condition')
+        ->will($this->returnSelf());
+
+    $select->expects($this->any())
+        ->method('execute')
+        ->will($this->returnValue($statement));
+
+    $database = $this->getMockBuilder('Drupal\Core\Database\Connection')
+        ->disableOriginalConstructor()
+        ->getMock();
+
+    $database->expects($this->any())
+        ->method('select')
+        ->will($this->returnValue($select));
+
+    $language_manager = $this->getMock('Drupal\Core\Language\LanguageManagerInterface');
+
+    $fieldTypePluginManager = $this->getMockBuilder('Drupal\Core\Field\FieldTypePluginManagerInterface')
+      ->disableOriginalConstructor()
+      ->getMock();
+
+    $this->container->set('entity.query', $factory);
+    $this->container->set('database', $database);
+    $this->container->set('cache.entity', $this->cacheBackend);
+    $this->container->set('language_manager', $language_manager);
+    $this->container->set('plugin.manager.field.field_type', $fieldTypePluginManager);
+
+    $this->entityStorage = $this->getMock('\Drupal\Core\Entity\EntityStorageInterface');
 
     $this->action = $this->actionManager->createInstance('rules_entity_property_fetch');
   }
@@ -46,7 +108,17 @@ class EntityPropertyFetchTest extends RulesEntityIntegrationTestBase {
    * @covers ::execute()
    */
   public function testActionExecution() {
-    $entity = $this->getMock('Drupal\Core\Entity\EntityInterface');
+    /*$property_values = array('test_property' => 'foo');
+    $entities = $this->entityStorage->expects($this->once())
+        ->method('loadProperties')
+        ->with($property_values);*/
+
+    $this->action->setContextValue('type', 'entity_test')
+      ->setContextValue('property', 'test_property')
+      ->setContextValue('value', 'llama')
+      ->setContextValue('limit', 10)
+      ->execute();
+    //$this->assertSame($entity, $this->action->getContextValue('provided'));
   }
 
 }
